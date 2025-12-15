7e39ef1ce00a28d8667e2d1583bd7cc7c594cd92
unlucky123 <66431033+unlucky123@users.noreply.github.com>
2025-12-15 18:53:33 +0100

    Add files via upload

---
+++ modificación 3/UF2.cFil.java
@@ -0,0 +1,56 @@
+package cFil;
+
+//alternativa: extends Thread
+public class cFil implements Runnable {
+
+  String aNomFil;
+
+  int aTemporitzacio;
+
+  public cFil (String pNomFil) {
+
+    aTemporitzacio = 500;
+
+    aNomFil = pNomFil;
+
+  }
+
+  public String gNomFil () {
+
+    return aNomFil;
+
+  }
+
+  public void sTemporitzacio (int pTemporitzacio) {
+
+    aTemporitzacio = pTemporitzacio;
+
+  }
+
+  public void run () {
+
+    System . out . println ("Iniciant execució procés " + aNomFil);
+
+    try {
+
+      for (int vComptador = 0; vComptador < 10; vComptador ++) {
+
+        Thread . sleep (aTemporitzacio);
+
+        System . out . println ("Despertant aturada " + vComptador + " procès " + aNomFil);
+
+      }
+
+    }
+
+    catch (InterruptedException pExcepcio) {
+
+      System . out . println ("Interrompent execució procès " + aNomFil);
+
+    }
+
+    System . out . println ("Acabant execució procès " + aNomFil);
+
+  }
+
+}
